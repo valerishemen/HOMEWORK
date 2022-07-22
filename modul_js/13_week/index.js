eb90f787ad333b64c48f500c80eafbbfae0cc84f
Valeri <lera250297@mail.ru>
2022-07-22 18:46:49 +0300

    13 неделя курса

---
+++ modul_js/13_week/index.js
@@ -0,0 +1,29 @@
+
+function showMessage(){
+    let name = prompt('Как тебя зовут?');
+alert(`Привет, ${name}!`)
+}
+
+function sum(){
+    let n1 =prompt ('x=');
+    let n2= prompt ('y=');
+    alert(Number(n1)+Number(n2));
+}
+
+function sbt(){
+    let n1 =prompt ('x=');
+    let n2= prompt ('y=');
+    alert(Number(n1)-Number(n2));
+}
+
+function mult(){
+    let n1 =prompt ('x=');
+    let n2= prompt ('y=');
+    alert(Number(n1)*Number(n2));
+}
+
+function seg(){
+    let n1 =prompt ('x=');
+    let n2= prompt ('y=');
+    alert(Number(n1)/Number(n2));
+}
